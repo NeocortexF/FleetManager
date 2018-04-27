53947a19c46f4bbc1ac37e45ad25f7cb78e09ee8
NeocortexF <eugeneshamkin@mail.ru>
2018-04-27 11:56:17 +0500

    explanation added

--- server/src/main/java/com/example/demo/beer/BeerController.java
+++ server/src/main/java/com/example/demo/beer/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
+//BeerController class to create an endpoint that filters out less-than-great beers
 @RestController
 public class BeerController {
     private BeerRepository repository;
@@ -15,6 +16,7 @@ public class BeerController {
         this.repository = repository;
     }
 
+    //тут http://localhost:8080/good-beers front будет ловить данные отсюда
     @GetMapping("/good-beers")
     @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000"})
     public Collection<Beer> goodBeers() {
--- server/src/main/java/com/example/demo/beer/BeerRepository.java
+++ server/src/main/java/com/example/demo/beer/BeerRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //class that leverages Spring Data to do CRUD on this entity.
+//Add a @RepositoryRestResource annotation to BeerRepository
+//to expose all its CRUD operations as REST endpoints.
 @RepositoryRestResource
 interface BeerRepository extends JpaRepository<Beer, Long> {
 }
