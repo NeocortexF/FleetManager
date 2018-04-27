04b2ba1e1e263d13feeec4455690a7644771a592
NeocortexF <eugeneshamkin@mail.ru>
2018-04-27 20:30:19 +0500

    added delete and add methods

--- server/src/main/java/eugene/neocortex/fm/beer/BeerController.java
+++ server/src/main/java/eugene/neocortex/fm/beer/BeerController.java
@@ -34,23 +34,44 @@ public class BeerController {
         return repository.findAll();
     }
 
-    @RequestMapping(value="/one/{id}")
+    @RequestMapping(value = "/one/{id}")
     @ResponseBody
-    public Beer oneBeer(@PathVariable("id") long id){
+    public Beer oneBeer(@PathVariable("id") long id) {
         return repository.findOne(id);
     }
 
-    @RequestMapping(value="/set/{id}/{name}")
+    @RequestMapping(value = "/update/{id}/{name}")
     @ResponseBody
-    public Beer setBeerName(@PathVariable("id") long id, @PathVariable("name") String newName){
-//        repository.findOne(id).setName(name);
-
-        Beer beerToUpdate = repository.getOne(id);
-        beerToUpdate.setName(newName);
-        repository.save(beerToUpdate);
-
-        System.out.println(repository.findOne(id) + "name has been changed to " + newName);
+    public Beer setBeerName(@PathVariable("id") long id, @PathVariable("name") String newName) {
+        if (newName != null) {
+            Beer beerToUpdate = repository.getOne(id);
+            beerToUpdate.setName(newName);
+            repository.save(beerToUpdate);
+        }
         return repository.findOne(id);
     }
 
+    @RequestMapping(value = "/delete/{id}")
+    @ResponseBody
+    public boolean removeBeer(@PathVariable("id") long id) {
+        if (repository.exists(id)) {
+            repository.delete(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @RequestMapping(value = "add/{name}")
+    @ResponseBody
+    public String addBeer(@PathVariable("name") String name) {
+        if (name != null && !name.isEmpty()){
+            Beer beerToSave = new Beer();
+            beerToSave.setName(name);
+            repository.save(beerToSave);
+            return beerToSave.toString();
+        } else {
+            return "Please enter the name of beer";
+        }
+    }
 }
