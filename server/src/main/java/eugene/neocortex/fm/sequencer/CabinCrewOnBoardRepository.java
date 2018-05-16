20b390d5de18b13f35427f3e413d746265747c0b
NeocortexF <eugeneshamkin@mail.ru>
2018-05-16 15:01:13 +0500

    cabin crew on board CRUD

--- server/src/main/java/eugene/neocortex/fm/sequencer/CabinCrewOnBoardRepository.java
+++ server/src/main/java/eugene/neocortex/fm/sequencer/CabinCrewOnBoardRepository.java
@@ -4,6 +4,9 @@ import eugene.neocortex.fm.sequencer.model.CabinCrewOnBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Collection;
+
 @RepositoryRestResource
 public interface CabinCrewOnBoardRepository extends JpaRepository<CabinCrewOnBoard, Long> {
+    Collection<CabinCrewOnBoard> findAllByFlightId(Long flightId);
 }
---
+++ server/src/main/java/eugene/neocortex/fm/sequencer/controller/CabinCrewOnBoardController.java
@@ -0,0 +1,72 @@
+package eugene.neocortex.fm.sequencer.controller;
+
+
+import eugene.neocortex.fm.sequencer.CabinCrewOnBoardRepository;
+import eugene.neocortex.fm.sequencer.model.CabinCrewOnBoard;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class CabinCrewOnBoardController {
+    private CabinCrewOnBoardRepository repository;
+
+    public CabinCrewOnBoardController(CabinCrewOnBoardRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = "cabin/byFlightId/{flightId}")
+    @ResponseBody
+    public Collection<CabinCrewOnBoard> findAllCabinCrewOnBoardByFlightId(
+            @PathVariable("flightId") Long id) {
+        return repository.findAllByFlightId(id);
+    }
+
+    @RequestMapping(value = "cabin/add/{flightId}/{name}/{position}")
+    @ResponseBody
+    public Boolean addCabinCrewMemberToFlight(@PathVariable("flightId") Long flightId,
+                                              @PathVariable("name") String name,
+                                              @PathVariable("position") String position) {
+
+        if (flightId != null) {
+            CabinCrewOnBoard cabinCrewOnBoard = new CabinCrewOnBoard();
+            cabinCrewOnBoard.setFlightId(flightId);
+            cabinCrewOnBoard.setName(name);
+            cabinCrewOnBoard.setPosition(position);
+            repository.save(cabinCrewOnBoard);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @RequestMapping(value = "cabin/update/{id}/{flightId}/{name}/{position}")
+    @ResponseBody
+    public CabinCrewOnBoard updateCabinCrewOnBoardById(@PathVariable("id") long id,
+                                               @PathVariable("flightId") long flightId,
+                                               @PathVariable("tailNumber") String name,
+                                               @PathVariable("description") String position) {
+
+        CabinCrewOnBoard cabinCrewOnBoardToUpdate = repository.getOne(id);
+        cabinCrewOnBoardToUpdate.setFlightId(flightId);
+        cabinCrewOnBoardToUpdate.setName(name);
+        cabinCrewOnBoardToUpdate.setPosition(position);
+        repository.save(cabinCrewOnBoardToUpdate);
+
+        return repository.findOne(id);
+    }
+
+    @RequestMapping(value = "cabin/delete/{id}")
+    @ResponseBody
+    public boolean removeCabinCrewMemberById(@PathVariable("id") long id) {
+        if (repository.exists(id)) {
+            repository.delete(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
