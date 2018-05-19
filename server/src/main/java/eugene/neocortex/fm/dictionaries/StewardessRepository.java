75205e09b90ead9da1a6f63f9c640752a82de9ab
NeocortexF <eugeneshamkin@mail.ru>
2018-05-19 19:30:41 +0500

    stewardess dict controller ready

--- server/src/main/java/eugene/neocortex/fm/dictionaries/StewardessRepository.java
+++ server/src/main/java/eugene/neocortex/fm/dictionaries/StewardessRepository.java
@@ -4,6 +4,12 @@ import eugene.neocortex.fm.dictionaries.model.Stewardess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Collection;
+
 @RepositoryRestResource
 public interface StewardessRepository extends JpaRepository<Stewardess, Long> {
+
+    Stewardess findByName(String name);
+
+    Collection<Stewardess> findAllByBrigade(Byte brigade);
 }
--- server/src/main/java/eugene/neocortex/fm/dictionaries/controller/StewardessController.java
+++ server/src/main/java/eugene/neocortex/fm/dictionaries/controller/StewardessController.java
@@ -1,4 +1,85 @@
 package eugene.neocortex.fm.dictionaries.controller;
 
+import eugene.neocortex.fm.dictionaries.StewardessRepository;
+import eugene.neocortex.fm.dictionaries.model.Stewardess;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
 public class StewardessController {
+    private StewardessRepository repository;
+
+    public StewardessController(StewardessRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("stewardess/all")
+    public Collection<Stewardess> getAllStewardess() {
+        return repository.findAll();
+    }
+
+
+    @RequestMapping(value = "stewardess/byId/{id}")
+    @ResponseBody
+    public Stewardess findStewardessById(@PathVariable("id") long id) {
+        return repository.findOne(id);
+    }
+
+
+    @RequestMapping(value = "stewardess/byName/{name}")
+    @ResponseBody
+    public Stewardess findStewardessByName(@PathVariable("name") String name) {
+        return repository.findByName(name);
+    }
+
+
+    @RequestMapping(value = "stewardess/allBySquad/{squad}")
+    @ResponseBody
+    public Collection<Stewardess> findStewardessByBrigade(@PathVariable("squad") Byte brigade) {
+        return repository.findAllByBrigade(brigade);
+    }
+
+    @RequestMapping(value = "stewardess/update/{id}/{name}/{position}/{squadron}")
+    @ResponseBody
+    public Stewardess updateStewardessById(@PathVariable("id") long id,
+                                           @PathVariable("name") String name,
+                                           @PathVariable("position") String position,
+                                           @PathVariable("brigade") Byte brigade) {
+
+        Stewardess stewardessToUpdate = repository.getOne(id);
+        stewardessToUpdate.setName(name);
+        stewardessToUpdate.setPosition(position);
+        stewardessToUpdate.setBrigade(brigade);
+        repository.save(stewardessToUpdate);
+
+        return repository.findOne(id);
+    }
+
+
+    @RequestMapping(value = "stewardess/delete/{id}")
+    @ResponseBody
+    public boolean removeStewardess(@PathVariable("id") long id) {
+        if (repository.exists(id)) {
+            repository.delete(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @RequestMapping(value = "stewardess/add/{name}/{position}/{brigade}")
+    @ResponseBody
+    public Boolean addStewardess(@PathVariable("name") String name,
+                                 @PathVariable("position") String position,
+                                 @PathVariable("brigade") Byte brigade) {
+
+        Stewardess stewardessToAdd = new Stewardess();
+        stewardessToAdd.setName(name);
+        stewardessToAdd.setPosition(position);
+        stewardessToAdd.setBrigade(brigade);
+        repository.save(stewardessToAdd);
+        return true;
+    }
 }
