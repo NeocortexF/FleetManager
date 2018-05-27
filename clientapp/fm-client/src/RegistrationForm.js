46e7a0fa701fd40f28a87620447e6c648aceb335
NeocortexF <eugeneshamkin@mail.ru>
2018-05-27 18:22:33 +0500

    committed class binding

--- clientapp/fm-client/src/RegistrationForm.js
+++ clientapp/fm-client/src/RegistrationForm.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 class RegistrationForm extends Component {
+
+	//дефолтное значение для e-mail пустая строка
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: ''
+		};
+		//что бы this ссылался не на функцию, а на экземпляр класса
+		//просто говоря, если не сделать то будет undefinded, а так
+		//будет ссылаться имено на форму регистрации и можно будет получать данные
+		this.handleEmailChange = this.handleEmailChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit() {
+		console.log('Form is submitted');
+	}
+
+	//что бы this нашего класса был доступен в нашей функции его нужно прибиндить,
+	//для этого прибиндим его в конструкторе
+	handleEmailChange() {
+		console.log('E-mail was changed', this);
+	}
+
 	render() {
 		return (
-			<div>Form</div>
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					placeholder="E-mail"
+					value={this.state.email}
+					onChange={this.handleEmailChange}
+				/>
+			</form>
 			);
 	}
 }
