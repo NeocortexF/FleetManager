b7f4fe3d4693d765ee76264cd6683ea8327d82f4
NeocortexF <eugeneshamkin@mail.ru>
2018-05-27 18:04:14 +0500

    Registration Form dev

--- clientapp/fm-client/src/App.js
+++ clientapp/fm-client/src/App.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Dropdown from './Dropdown.js';
-import Header from './Header.js';
+import RegistrationForm from './RegistrationForm.js';
 
 // как будто данные получили от бэк энд. Делают один умный компонент остальные вложенные тупые
 // умный App, а Header будет тупой
-const menu = [
-  {
-    link: '/articles',
-    label: 'Articles'
-  },
-  {
-    link: '/contacts',
-    label: 'Contacts'
-  },
-  {
-    link: '/posts',
-    label: 'Posts'
-  }
-];
+// const menu = [
+//   {
+//     link: '/articles',
+//     label: 'Articles'
+//   },
+//   {
+//     link: '/contacts',
+//     label: 'Contacts'
+//   },
+//   {
+//     link: '/posts',
+//     label: 'Posts'
+//   }
+// ];
 
 
 class App extends Component {
   render() {
     return (
       <div className = 'App'>
-        <Header items={menu} />
+        <RegistrationForm />
       </div>
     );
   }
---
+++ clientapp/fm-client/src/RegistrationForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RegistrationForm extends Component {
+	render() {
+		return (
+			<div>Form</div>
+			);
+	}
+}
+
+export default RegistrationForm;
